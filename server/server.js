dadef50edf88b8dec6bf25bb33ff6365ea4ab401
Claudia N <62987170+cnicolae94@users.noreply.github.com>
2023-03-11 12:38:59 +0200

    added sequelize

--- server/server.js
+++ server/server.js
@@ -1,7 +1,18 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
+
+//postgres username admin password
+const app = express();
+
+let router = express.Router();
+
 app.use(bodyParser.json());
 app.use(cors());
+
+app.use("/api", router);
+
+router.route("/getArtists")
+
+app.get("/api", (req, res) => {});
